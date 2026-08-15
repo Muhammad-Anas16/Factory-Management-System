3d8a4dbbb334f032e1295726193b525b08cfa976
Muhammad Anas <anas.dev122@gmail.com>
2026-08-15 14:30:30 +0400

    added user table and models

---
+++ backend/src/model/user/user.model.js
@@ -0,0 +1,94 @@
+import db from "../../consfig/db.js";
+
+export const createUserTable = () => {
+  const sql = ` CREATE TABLE IF NOT EXIST users (
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+      name TEXT NOT NULL,
+      username TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL,
+
+      role TEXT NOT NULL DEFAULT 'user',
+
+      allowedPages TEXT NOT NULL DEFAULT '[]',
+
+      canAdd INTEGER NOT NULL DEFAULT 0,
+      canEdit INTEGER NOT NULL DEFAULT 0,
+      canDelete INTEGER NOT NULL DEFAULT 0,
+
+      status TEXT NOT NULL DEFAULT 'active',
+
+      createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+      updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`;
+
+  db.exec(sql);
+  console.log("User Table Created Successfully!");
+};
+
+export const createUser = (user) => {
+  const statment = db.prepare(`
+        INSERT INTO users (
+        name, username, password, role, allowedPages, canAdd, canEdit, canDelete, status
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`);
+  return statment.run(
+    user.name,
+    user.username,
+    user.password,
+    user.role || "user",
+    user.allowedPages || [],
+    user.canAdd ? 1 : 0,
+    user.canEdit ? 1 : 0,
+    user.canDelete ? 1 : 0,
+    user.status || "active",
+  );
+};
+
+export const findUserByUsername = (username) => {
+  const statement = db.prepare("SELECT * FROM users WHERE username = ?");
+  return statement.get(username);
+};
+
+export const findUserById = (id) => {
+  const statement = db.prepare("SELECT * FROM users WHERE id = ?");
+
+  return statement.get(id);
+};
+
+export const getAllUsers = () => {
+  const statement = db.prepare(
+    "SELECT id, name, username, role, allowedPages, canAdd, canEdit, canDelete, status, createdAt, updatedAt FROM users ORDER BY id DESC",
+  );
+
+  return statement.all();
+};
+
+export const updateUser = (user) => {
+  const statment = db.prepare(`
+        UPDATE users SET (
+        name = ?, username = ?, role = ?, allowedPages = ?, canAdd = ?, canEdit = ?, canDelete = ?, status = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?
+    )`);
+  return statment.run(
+    user.name,
+    user.username,
+    user.role || "user",
+    user.allowedPages || [],
+    user.canAdd ? 1 : 0,
+    user.canEdit ? 1 : 0,
+    user.canDelete ? 1 : 0,
+    user.status || "active",
+  );
+};
+
+export const updateUserPassword = (id, hashedPassword) => {
+  const statment = db.prepare(`
+        UPDATE users SET password = ?, updatedAt = CURRENT_TIMESTAMP WHERE = ?
+    `);
+  return statment.run(hashedPassword, id);
+};
+
+export const deleteUser = (id) => {
+  const statment = db.prepare(`DELETE FROM users WHERE id = ?`);
+  return statment.run(id);
+};
